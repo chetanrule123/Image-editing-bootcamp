075300c2161400560c04432e2d83377839fa798d
chetan <chetanrule365@gmail.com>
2020-10-01 14:34:31 +0530

    Updates

---
+++ precache-manifest.8f704b65413d1550b43db24257ec0478.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cff9be66476b20f65f07dbb0da97508",
+    "url": "/Image-editing-bootcamp/index.html"
+  },
+  {
+    "revision": "ada4735651e26d7483a8",
+    "url": "/Image-editing-bootcamp/static/css/main.a1ea719f.chunk.css"
+  },
+  {
+    "revision": "79a7ac96507f585da5dc",
+    "url": "/Image-editing-bootcamp/static/js/2.9cdce9d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Image-editing-bootcamp/static/js/2.9cdce9d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada4735651e26d7483a8",
+    "url": "/Image-editing-bootcamp/static/js/main.86bf38ba.chunk.js"
+  },
+  {
+    "revision": "343f611c42ef46f7ad94",
+    "url": "/Image-editing-bootcamp/static/js/runtime-main.6780e095.js"
+  },
+  {
+    "revision": "66be0c6c1c8f2104858abff0bbde4a78",
+    "url": "/Image-editing-bootcamp/static/media/bg.66be0c6c.jpg"
+  },
+  {
+    "revision": "cfe07be359453f662e75320cb95335fa",
+    "url": "/Image-editing-bootcamp/static/media/empty_illustration.cfe07be3.jpg"
+  },
+  {
+    "revision": "f4eb78769af2659fdd240e3c91b3a90f",
+    "url": "/Image-editing-bootcamp/static/media/login_illustration.f4eb7876.jpg"
+  },
+  {
+    "revision": "bcc71b220e9fe1ddb16c5348ef6d1a8f",
+    "url": "/Image-editing-bootcamp/static/media/logo.bcc71b22.jpg"
+  }
+]);
